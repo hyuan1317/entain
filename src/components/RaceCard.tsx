b5fceba72e580697079b8171643cd966e5491802
hyuan1317 <kool717alex@gmail.com>
2022-02-22 11:00:14 +1100

    feat(RaceCard): RaceCard with counting down feature

---
+++ src/components/RaceCard.tsx
@@ -0,0 +1,99 @@
+import React, { FC, useState, useEffect, useMemo } from 'react';
+import styled from 'styled-components/native';
+import { RaceInfo } from '../services/raceApi';
+
+interface Props {
+  race: RaceInfo;
+}
+
+const Wrapper = styled.View`
+  width: 100%;
+  border-width: 2px;
+  border-color: gray;
+  margin-bottom: 15px;
+  border-radius: 8px;
+  padding: 15px;
+`;
+const Title = styled.Text`
+  font-size: 20px;
+`;
+const Content = styled.Text`
+  font-size: 18px;
+`;
+const FlexRow = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 4px;
+`;
+
+interface CountingTimeType {
+  min: number;
+  sec: number;
+}
+
+function formatTimeRemaining(seconds: number) {
+  let remainingSecs = seconds - new Date().getTime() / 1000;
+
+  if (remainingSecs <= 0) {
+    return {
+      min: 0,
+      sec: 0,
+    };
+  }
+
+  const countingInMin = Math.floor(remainingSecs / 60);
+  remainingSecs = remainingSecs - countingInMin * 60;
+  const countingInSec = Math.floor(remainingSecs);
+  return {
+    min: countingInMin,
+    sec: countingInSec,
+  };
+}
+
+const RaceCard: FC<Props> = props => {
+  const { race } = props;
+  const {
+    meeting_name,
+    race_number,
+    advertised_start: { seconds },
+  } = race;
+
+  const initialCountingTime = useMemo(() => {
+    return formatTimeRemaining(seconds);
+  }, [seconds]);
+
+  const [countingTime, setCountingTime] =
+    useState<CountingTimeType>(initialCountingTime);
+
+  useEffect(() => {
+    const counter = setInterval(() => {
+      const now = new Date().getTime() / 1000; // milliseconds to seconds
+      if (now >= seconds) {
+        clearInterval(counter);
+      } else {
+        const newCountingTime = formatTimeRemaining(seconds);
+        setCountingTime(newCountingTime);
+      }
+    }, 1000);
+  }, [seconds]);
+
+  return (
+    <Wrapper>
+      <FlexRow>
+        <Title>Meeting Name:</Title>
+        <Content>{meeting_name}</Content>
+      </FlexRow>
+      <FlexRow>
+        <Title>Race Number:</Title>
+        <Content>{race_number}</Content>
+      </FlexRow>
+      <FlexRow>
+        <Title>Starting in:</Title>
+        <Content>{`${countingTime.min}mins ${countingTime.sec}secs `}</Content>
+      </FlexRow>
+    </Wrapper>
+  );
+};
+
+export default RaceCard;
---
+++ src/screens/Home.tsx
@@ -0,0 +1,41 @@
+import React, { FC, useEffect } from 'react';
+import styled from 'styled-components/native';
+import { useSelector, useDispatch } from 'react-redux';
+import { FlatList } from 'react-native';
+import { RootState } from '../reducers';
+import { getNextRaceList } from '../actions/race';
+import RaceCard from '../components/RaceCard';
+import { RaceInfo } from '../services/raceApi';
+
+const Wrapper = styled.View`
+  flex: 1;
+  padding: 0 10px;
+`;
+const Title = styled.Text`
+  text-align: center;
+  font-size: 32px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Home: FC = () => {
+  const dispatch = useDispatch();
+  const { raceList } = useSelector((state: RootState) => state.race);
+
+  useEffect(() => {
+    dispatch(getNextRaceList());
+  }, [dispatch]);
+
+  return (
+    <Wrapper>
+      <Title>Next to go</Title>
+      <FlatList
+        data={raceList}
+        keyExtractor={(item: RaceInfo) => item.race_id}
+        renderItem={({ item }: { item: RaceInfo }) => <RaceCard race={item} />}
+      />
+    </Wrapper>
+  );
+};
+
+export default Home;
